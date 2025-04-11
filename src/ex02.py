b85a470fc24103e7b7af4d2f050f8c7742c621b4
bac0nb0yy <kyuma@pm.me>
2025-04-11 23:36:17 +0200

    requirements changed

--- src/ex02.py
+++ src/ex02.py
@@ -1,14 +1,27 @@
 import matplotlib
 from matplotlib import pyplot as plt
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, transpile
+from qiskit.visualization import plot_histogram
+from qiskit_aer import AerSimulator
 
+SHOTS = 500
 matplotlib.use("qtagg")
 
 
 def main():
-    qc = QuantumCircuit(1)
+    qc = QuantumCircuit(1, 1)
     qc.h(0)
-    qc.draw("mpl")
+    qc.measure(0, 0)
+
+    simulator = AerSimulator()
+    qc = transpile(qc, simulator)
+
+    result = simulator.run(qc, shots=SHOTS).result()
+    counts = result.get_counts(qc)
+    normalized_counts = {state: count / SHOTS for state, count in counts.items()}
+
+    plot_histogram(normalized_counts)
+    plt.ylabel("Probabilities")
     plt.show()
 
 
